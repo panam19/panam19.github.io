0015ee0539bd46f4df693803f26195b56f5efd66
panam19 <kitabersama1900@gmail.com>
2025-05-13 01:28:50 +0700

    Delete _next/static directory

--- _next/static/Inm7ttxvcbk4JDqQmdvtg/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fcommunity","\u002Fdevelopers","\u002Fecosystem","\u002Fgrants-program","\u002Fonuschain"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
